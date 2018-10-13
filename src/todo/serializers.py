46ebd23b52c2f8c45d5ea86f9316ba11e4a0b4dd
Mohammad Zeeshan <zee7han@gmail.com>
2018-10-13 18:39:35 +0530

    Create all views and serializer for todo functionality.

---
+++ src/todo/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from todo.models import TodoItem
+
+
+class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.ReadOnlyField()
+    class Meta:
+        model = TodoItem
+        fields = ('url', 'title', 'completed', 'order')
